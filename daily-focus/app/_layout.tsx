522fe9dadf5f8b1e9cccfd839f2997b299560456
Minh Phuong Dang <minhphuonglcby@gmail.com>
2025-10-21 17:56:37 +0700

    fix export apk file

--- daily-focus/app/_layout.tsx
+++ daily-focus/app/_layout.tsx
@@ -1,13 +1,11 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
-
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import { auth } from "@/firebase/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { View, ActivityIndicator } from "react-native";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -15,29 +13,37 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [initialRoute, setInitialRoute] = useState<string | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setInitialRoute(user ? "(tabs)" : "login-register/login-activity");
+    });
+    return unsubscribe;
+  }, []);
+
+  if (!initialRoute) {
+    // Hiển thị loading trong lúc check auth
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack screenOptions={{ headerShown: false }} initialRouteName={initialRoute}>
         <Stack.Screen
           name="login-register/login-activity"
-          options={{
-            title: "Login",
-            presentation: "modal",
-          }}
+          options={{ title: "Login", presentation: "modal" }}
         />
         <Stack.Screen
           name="login-register/register-activity"
-          options={{
-            title: "Register",
-            presentation: "modal",
-          }}
+          options={{ title: "Register", presentation: "modal" }}
         />
-        <Stack.Screen
-          name="(tabs)"
-        />
+        <Stack.Screen name="(tabs)" />
       </Stack>
-    
       <StatusBar style="auto" />
     </ThemeProvider>
   );
